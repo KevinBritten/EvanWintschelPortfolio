bf6406fd10cbf6bd563d6530cd136466b4835799
Kevin <kevinjbritten@gmail.com>
2020-01-16 13:54:29 -0500

    bringing file back to webflow version

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,7 @@
+import '../styles/styles.css'
+
+console.log("Hello World from your main file!");
+
+if (module.hot) {
+    module.hot.accept()
+}
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const workboxPlugin = require('workbox-webpack-plugin');
+
+const postCSSPlugins = [
+	require('postcss-import'),
+	require('postcss-mixins'),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('postcss-hexrgba'),
+    require('autoprefixer')
+]
+
+let cssConfig = {
+	test: /\.css$/i,
+	use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins }}]
+}
+
+module.exports = {
+	mode: 'development',
+	entry: './app/assets/scripts/app.js',
+	devServer: {
+        before: function(app, server){
+            server._watch('./app/*.html');
+          },
+        contentBase: path.join(__dirname, "app"),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'},
+	plugins: [
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({ filename: 'main.[chunkhash].css' }),
+		new workboxPlugin.GenerateSW({
+			swDest: 'sw.js',
+			clientsClaim: true,
+			skipWaiting: false
+		})
+	],
+
+
+
+	module: {
+		rules: [
+			{
+				test: /.(js|jsx)$/,
+				include: [path.resolve(__dirname, 'app/assets/scripts')],
+				exclude: /(node_modules)/,
+				loader: 'babel-loader'
+			},
+		cssConfig
+		]
+	},
+
+	optimization: {
+		minimizer: [new TerserPlugin()],
+
+		splitChunks: {
+			cacheGroups: {
+				vendors: {
+					priority: -10,
+					test: /[\\/]node_modules[\\/]/
+				}
+			},
+
+			chunks: 'async',
+			minChunks: 1,
+			minSize: 30000,
+			name: true
+		}
+	}
+};

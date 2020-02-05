295688054d99cc2c73470896eb48a92eafb36dd3
Kevin <kevinjbritten@gmail.com>
2020-02-05 16:50:50 -0500

    seperated dev and build tasks, updated build task to copy images, use htmlloader, use minicssextract

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,9 @@
 import '../styles/styles.css';
 import BackgroundImageChange from './modules/BackgroundImageChange';
-import AboutSection from './modules/AboutSection';
+// import AboutSection from './modules/AboutSection';
 
 new BackgroundImageChange();
-new AboutSection();
+// new AboutSection();
 
 if (module.hot) {
 	module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const workboxPlugin = require('workbox-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
 
 const postCSSPlugins = [
 	require('postcss-import'),
@@ -13,36 +17,36 @@ const postCSSPlugins = [
 	require('autoprefixer')
 ];
 
+class RunAfterCompile {
+	apply(compiler) {
+		compiler.hooks.done.tap('Copy Images', function() {
+			fse.copySync('./app/assets/images', './dist/assets/images');
+		});
+	}
+}
+
 let cssConfig = {
 	test: /\.css$/i,
-	use: [ 'style-loader', 'css-loader?url=false', { loader: 'postcss-loader', options: { plugins: postCSSPlugins } } ]
+	use: [
+		// 'css-loader?url=false',
+		'css-loader',
+		{ loader: 'postcss-loader', options: { plugins: postCSSPlugins } }
+	]
 };
 
-module.exports = {
-	mode: 'development',
+let config = {
 	entry: [ 'babel-polyfill', './app/assets/scripts/app.js' ],
-	output: {
-		publicPath: ''
-	},
-	// "externals": {
-	// 	"fs": "require('fs')",
-	//  },
-	devServer: {
-		before: function(app, server) {
-			server._watch('./app/*.html');
-		},
-		contentBase: path.join(__dirname, 'app'),
-		hot: true,
-		port: 3000,
-		host: '0.0.0.0'
-	},
 	plugins: [
 		new webpack.ProgressPlugin(),
-		new MiniCssExtractPlugin({ filename: 'main.[chunkhash].css' }),
+
 		new workboxPlugin.GenerateSW({
 			swDest: 'sw.js',
 			clientsClaim: true,
 			skipWaiting: false
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './app/index.html'
 		})
 	],
 	node: {
@@ -71,23 +75,61 @@ module.exports = {
 			},
 			cssConfig
 		]
-	},
-
-	optimization: {
-		minimizer: [ new TerserPlugin() ],
-
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
-
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
 	}
+
+	// optimization: {
+	// 	minimizer: [ new TerserPlugin() ],
+
+	// 	splitChunks: {
+	// 		cacheGroups: {
+	// 			vendors: {
+	// 				priority: -10,
+	// 				test: /[\\/]node_modules[\\/]/
+	// 			}
+	// 		},
+
+	// 		chunks: 'async',
+	// 		minChunks: 1,
+	// 		minSize: 30000,
+	// 		name: true
+	// 	}
+	// }
 };
+
+if (currentTask === 'dev') {
+	cssConfig.use.unshift('style-loader');
+	config.mode = 'development';
+	config.devServer = {
+		before: function(app, server) {
+			server._watch('./app/*.html');
+		},
+		contentBase: path.join(__dirname, 'app'),
+		hot: true,
+		port: 3000,
+		host: '0.0.0.0'
+	};
+	config.output = {
+		filename: 'bundled.js',
+		path: path.resolve(__dirname, 'app')
+	};
+}
+
+if (currentTask === 'build') {
+	cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+	config.mode = 'production';
+	config.output = {
+		filename: '[name].[chunkhash].js',
+		chunkFilename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'dist')
+	};
+	config.optimization = {
+		splitChunks: { chunks: 'all' }
+	};
+	config.plugins.unshift(
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' }),
+		new RunAfterCompile()
+	);
+}
+
+module.exports = config;

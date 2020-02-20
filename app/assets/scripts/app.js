c1b54b33ef98f7c66022dde10e2b2f4931f3698b
Kevin <kevinjbritten@gmail.com>
2020-02-20 08:18:14 -0500

    removed obsolete album populator file

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,12 +1,10 @@
 import '../styles/styles.css';
-import AlbumPopulator from './modules/AlbumPopulator';
 import BackgroundImageChange from './modules/BackgroundImageChange';
 import AboutSection from './modules/AboutSection';
 import PageSetup from './modules/PageSetup';
 import EmailCopy from './modules/EmailCopy';
 import Lightbox from './modules/Lightbox';
 
-// new AlbumPopulator();
 new Lightbox();
 new BackgroundImageChange();
 new AboutSection();

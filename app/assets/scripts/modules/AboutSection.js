fd925050981269a6dce59d29e3a3f087e5a7adce
Kevin <kevinjbritten@gmail.com>
2020-05-22 17:18:54 -0400

    renamed about to content, added print overlay

--- app/assets/scripts/modules/AboutSection.js
+++ app/assets/scripts/modules/AboutSection.js
@@ -1,35 +1,57 @@
 class AboutSection {
     constructor() {
         this.aboutBtn = document.querySelector('#about-button');
-        this.closeBtn = document.querySelector('.about-overlay__close-button');
+        this.printsBtn = document.querySelector('#prints-button');
+        this.closeBtn = document.querySelector('.content-overlay__close-button');
         this.contentHeading = document.querySelector('.content-area__heading');
         this.contentSectionHeight = document.querySelector('.content-area').scrollHeight;
         this.contentSection = document.querySelector('.content-area__box');
-        this.aboutOverlay = document.querySelector('.about-overlay');
+        this.contentOverlay = document.querySelector('.content-overlay');
         this.albumsList = document.querySelector('.content-area__list');
-        this.aboutEscape = function() {
+        this.contentText = document.querySelector('.content-overlay__text');
+        this.contentEscape = function() {
             if (event.keyCode === 27) {
-                this.toggleAboutSection();
+                this.toggleSection();
             }
         };
-        this.aboutEscapeBinded = this.aboutEscape.bind(this);
+        this.contentEscapeBinded = this.contentEscape.bind(this);
         this.events();
     }
 
     events() {
-        this.aboutBtn.addEventListener('click', () => this.toggleAboutSection());
-        this.closeBtn.addEventListener('click', () => this.toggleAboutSection());
+        this.aboutBtn.addEventListener('click', (e) => this.openContent(e));
+        this.printsBtn.addEventListener('click', (e) => this.openContent(e));
+        this.closeBtn.addEventListener('click', (e) => this.toggleSection());
     }
 
-    toggleAboutSection() {
+    openContent(e) {
+        let capsTitle = e.target.innerText;
+        let title = capsTitle.charAt(0) + capsTitle.slice(1).toLowerCase();
+        let contentTexts = {
+            About: 'string about bakjwdbkajwbstring about bakjwdbkajwbdkajwbdkstring about bakjwdbkajwbdkajwbdkstring about bakjwdbkajwbdkajwbdkstring about bakjwdbkajwbdkajwbdkdkajwbdkjaw',
+            Prints: 'string pringsslkeflskef bakjwdbkajwbstring aefjsl bakjwdbkajwbdkajwbdkstring about bakjwdbkajwbdkajwbdkstring about bakjwdbkajwbdkajwbdkstring about bakjwdbkajwbdkajwbdkdkajwbdkjaw'
+        };
         this.contentSection.style.minHeight = `${this.contentSectionHeight}px`;
-        this.aboutOverlay.classList.toggle('about-overlay--is-visible');
-        this.albumsList.classList.toggle('content-area__list--is-hidden');
-        if (this.aboutOverlay.classList.contains('about-overlay--is-visible')) {
-            document.addEventListener('keydown', this.aboutEscapeBinded);
-            this.contentHeading.innerText = 'About';
+        if (
+            title === this.contentHeading.innerText &&
+            this.contentOverlay.classList.contains('content-overlay--is-visible')
+        ) {
+            this.toggleSection();
+        } else if (!this.contentOverlay.classList.contains('content-overlay--is-visible')) {
+            document.addEventListener('keydown', this.contentEscapeBinded);
+            this.contentHeading.innerText = title;
+            this.contentText.innerText = contentTexts[`${title}`];
+            this.toggleSection();
         } else {
-            document.removeEventListener('keydown', this.aboutEscapeBinded);
+            this.contentHeading.innerText = title;
+            this.contentText.innerText = contentTexts[`${title}`];
+        }
+    }
+    toggleSection() {
+        this.contentOverlay.classList.toggle('content-overlay--is-visible');
+        this.albumsList.classList.toggle('content-area__list--is-hidden');
+        if (!this.contentOverlay.classList.contains('content-overlay--is-visible')) {
+            document.removeEventListener('keydown', this.contentEscapeBinded);
             this.contentHeading.innerText = 'Albums';
         }
     }

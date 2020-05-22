319a3210603b8a04a10731b6f9f55518bd65a6d5
Kevin <kevinjbritten@gmail.com>
2020-05-22 16:00:42 -0400

    added overflow to thumbnails, renamed prints, added images and albums for test

--- app/assets/scripts/modules/ImageList.js
+++ app/assets/scripts/modules/ImageList.js
@@ -1,5 +1,5 @@
 class ImageList{
 						constructor(){
-					this.list = {"Gumpers Gumping":["07980022.JPG","07990014_1-bg.jpg","08000002_1-bg.jpg","08000011_1-bg.JPG"],"The Adventures of Fern":["08010011.JPG","08040014_1-bg.JPG","08040018-bg.JPG","08040025_2-bg.JPG","08040027.JPG"],"Vegetals":["08060014_1-bg.jpg","08060017-bg.JPG","08060021_1-bg.JPG","08060029_1-bg.jpg","08060033.JPG","16550003-bg.jpg"]}     }
+					this.list = {"Gumpers Gumping":["07980022.JPG","08000002_1-bg.jpg","08000011_1-bg.JPG"],"The Adventures of Fern":["08010011.JPG"],"Vegemite":["08040014_1-bg 2.JPG","08040014_1-bg.JPG","08040027 10.JPG","08040027 2.JPG","08040027 3.JPG","08040027 4.JPG","08040027 5.JPG","08040027 6.JPG","08040027 7.JPG","08040027 8.JPG","08040027 9.JPG","08040027.JPG","08060014_1-bg 2.jpg","08060014_1-bg.jpg","08060033 2.JPG","08060033 3.JPG","08060033 4.JPG","08060033 5.JPG","08060033 6.JPG","08060033.JPG","16550003-bg 2.jpg","16550003-bg.jpg","hello 2.JPG","hello.JPG"],"sep":["07990014_1-bg.jpg"],"sup":["08040018-bg.JPG","08040025_2-bg.JPG"],"sus":["08060017-bg.JPG","08060021_1-bg.JPG","08060029_1-bg.jpg"]}     }
 				}
 				export default ImageList

96ea03e47ea023cc23594eab92c888567bad16f0
Kevin <kevinjbritten@gmail.com>
2020-05-28 21:10:20 -0400

    removed page autoscroll

--- app/assets/scripts/modules/PageSetup.js
+++ app/assets/scripts/modules/PageSetup.js
@@ -1,7 +1,21 @@
 class PageSetup {
     constructor() {
         this.setDimensions();
+        this.disableKeyScroll();
         window.onload = this.pageOpacity;
+    }
+
+    disableKeyScroll() {
+        window.addEventListener(
+            'keydown',
+            function(e) {
+                // space and arrow keys
+                if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+                    e.preventDefault();
+                }
+            },
+            false
+        );
     }
 
     pageOpacity() {
